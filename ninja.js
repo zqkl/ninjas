99fc22491a21c88ff007c21da6217abc995df27e
Jacob Anile <jacobanile1@gmail.com>
2023-01-10 18:10:47 -0800

    initial commit

---
+++ ninja.js
@@ -0,0 +1,28 @@
+class Ninja { 
+    constructor(name,health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName(){
+        console.log(`${this.name}`)
+    }
+
+    showStats(){
+        console.log(`${this.name} has ${this.speed} speed, ${this.strength} strength , and ${this.health} health.`)
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+}
+
+
+const ninja1 = new Ninja("Carson",100);
+ninja1.sayName();
+ninja1.drinkSake();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
